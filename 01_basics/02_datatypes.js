1592730dd81b43e1d5e9cac74e84fa859987d004
Shivani Bhati <shivani4960@gmail.com>
2024-09-30 17:42:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // used to treat all js code as newer version
+
+//years ago nodejs engine was in browsers but now its independent
+
+//alert(3+3) //we are using nodejs not browser so dont use it like this
+
+let name = "shivani" //string
+let age = 21  //number
+let loggedin = true  //bollean
+let state     //undegined
+let temperature = null //null => a stand alone meaning
+
+//types of datatype
+/* number - range from 2^1 to 2^53
+   bigint
+   boolean
+   string
+   null => object
+   undegined 
+   symbol => unique
+*/
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
