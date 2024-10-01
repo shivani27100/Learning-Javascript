fc1ef0346b41e728d37fbce744ebbf38353b381e
Shivani Bhati <shivani4960@gmail.com>
2024-10-01 13:11:42 +0000

    operation and conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,49 @@
+// let score = 33
+// console.log(typeof score);
+// op=>number
+
+let score = "33"
+// console.log(typeof score);
+// op=>string
+
+//changing a string in number
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber); //op => number
+
+//if input is 33abc than it is changed to number the op will be NaN(NOT A NUMBER)
+// true =>1; false=>0
+
+let loggedin="shivani"
+let bolleanloggedin= Boolean(loggedin)
+// console.log(bolleanloggedin);
+//"shivani"=> TRUE
+// ""(empty string) => false
+
+
+//********************************operations*******************************
+let val = 33
+let negval = -val 
+console.log(negval);
+
+//basic operations
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2%2)
+console.log(2**3) // this means 2 ^3
+
+let str1= "hello"
+let str2= "shivani"
+let str3= str1+str2
+console.log(str3) //helloshivani
+
+log
+
+
+
+
+
+
+
