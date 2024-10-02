ea93f13309d8a112d8bfe9f6aa603809a3fce223
Shivani Bhati <shivani4960@gmail.com>
2024-10-02 14:10:29 +0000

    operators and datatypes

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -2,44 +2,55 @@
 // console.log(typeof score);
 // op=>number
 
-let score = "33"
+// let score = "33"
 // console.log(typeof score);
 // op=>string
 
 //changing a string in number
 
-let valueInnumber = Number(score)
+// let valueInnumber = Number(score)
 // console.log(typeof valueInnumber); //op => number
 
 //if input is 33abc than it is changed to number the op will be NaN(NOT A NUMBER)
 // true =>1; false=>0
 
-let loggedin="shivani"
-let bolleanloggedin= Boolean(loggedin)
+// let loggedin="shivani"
+// let bolleanloggedin= Boolean(loggedin)
 // console.log(bolleanloggedin);
 //"shivani"=> TRUE
 // ""(empty string) => false
 
 
 //********************************operations*******************************
-let val = 33
-let negval = -val 
-console.log(negval);
+// let val = 33
+// let negval = -val 
+// console.log(negval);
 
-//basic operations
-console.log(2+2)
-console.log(2-2)
-console.log(2/2)
-console.log(2*2)
-console.log(2%2)
-console.log(2**3) // this means 2 ^3
+// //basic operations
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2**3) // this means 2 ^3
 
-let str1= "hello"
-let str2= "shivani"
-let str3= str1+str2
-console.log(str3) //helloshivani
+// let str1= "hello"
+// let str2= "shivani"
+// let str3= str1+str2
+// console.log(str3) //helloshivani
 
-log
+console.log("1" +2) //12 as string
+console.log(1 + "2") // 12 string
+
+console.log("1" + 2 + 2) //122 all will change to string and concatenate. 
+
+console.log(1 + 2 + "3") // 33 as 1+2 are number they wil add as 3 and than 3 is a string so so it will concatinate it becomes 33.
+
+
+
+
+
+
 
 
 
