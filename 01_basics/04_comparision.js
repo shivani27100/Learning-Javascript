018c45a01d18664bc5c55aef7d979aeb654bcb5c
Shivani Bhati <shivani4960@gmail.com>
2024-10-02 14:44:45 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+console.log(2>1);
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+//al the ans will come as true or false
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === its a strict check it checks the equality as well as datatype and anything is different it returns false.
+
+console.log("2" === 2) //
